5dc46f43633ddde8d185a90c8cff54dcf706228f
Sho Nitta <besokko@gmail.com>
2023-03-19 01:04:57 +0900

    wait timing change

--- mpv.bat
+++ mpv.bat
@@ -4,9 +4,11 @@ tasklist | find /i "mpv.exe"
 
 if errorlevel 0 (
 	for /F "tokens=2 skip=3" %%i in ('tasklist /FI "IMAGENAME eq mpv.exe"') do (
-		if errorlevel 0 cscript %APPDATA%\mpv\mpv_quit.vbs %%i
+		if errorlevel 0 (
+			cscript %APPDATA%\mpv\mpv_quit.vbs %%i
+			powershell Start-Sleep -m 500
+		)
 	)
-	powershell Start-Sleep -Milliseconds 500
 )
 
 start "" "C:\ProgramData\chocolatey\lib\mpv.install\tools\mpv.exe" %*
--- mpv_quit.vbs
+++ mpv_quit.vbs
@@ -3,8 +3,8 @@ set pid = WScript.Arguments
 
 if pid.count <> 0 then
 	WSShell.AppActivate(Cint(pid(0)))
-	'WScript.Sleep 100 '終了失敗する場合はSleepを挟む
 	WSShell.SendKeys "%{F4}" 'ALT+F4
+	' WScript.Sleep 750 '終了失敗する場合はSleepを挟む
 else
 	Wscript.echo "PID NOT FOUND"
 end if
