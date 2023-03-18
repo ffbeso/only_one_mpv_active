aae870624d9207963e529580c622a6ea2b7a64b3
Sho Nitta <besokko@gmail.com>
2023-03-19 01:27:04 +0900

    Wait Timing Change

--- mpv.bat
+++ mpv.bat
@@ -6,9 +6,10 @@ if errorlevel 0 (
 	for /F "tokens=2 skip=3" %%i in ('tasklist /FI "IMAGENAME eq mpv.exe"') do (
 		if errorlevel 0 (
 			cscript %APPDATA%\mpv\mpv_quit.vbs %%i
-			powershell Start-Sleep -m 500
+			
 		)
 	)
+	powershell Start-Sleep -m 750
 )
 
 start "" "C:\ProgramData\chocolatey\lib\mpv.install\tools\mpv.exe" %*
