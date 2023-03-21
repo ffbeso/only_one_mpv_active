8ac2e17c8daac420ff7bda5245f20c217c2dd9b6
Sho Nitta <besokko@gmail.com>
2023-03-22 00:14:32 +0900

    Fixed launch failure

--- mpv.bat
+++ mpv.bat
@@ -1,20 +1,15 @@
 @echo off
 
-:killmpv
+:quit_mpv
 
 tasklist | find /i "mpv.exe"
 
 if %errorlevel% == 0 (
 	for /F "tokens=2 skip=3" %%i in ('tasklist /FI "IMAGENAME eq mpv.exe"') do (
 		if %errorlevel% == 0 (
-			cscript %APPDATA%\mpv\mpv_quit.vbs %%i
-			goto lunchmpv
+			cscript %APPDATA%\mpv\mpv_quit.vbs %%i > nul
+			goto quit_mpv
 		)
 	)
 )
-
-:lunchmpv
-
 start "" "C:\ProgramData\chocolatey\lib\mpv.install\tools\mpv.exe" %*
-
-
