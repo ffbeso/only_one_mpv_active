fe8da52cd6d89a05bedf9a7fafb7ae82108ecfcd
Sho Nitta <besokko@gmail.com>
2023-03-21 18:17:04 +0900

    Fixed an occasional launch failure.

--- mpv.bat
+++ mpv.bat
@@ -1,15 +1,20 @@
 @echo off
 
+:killmpv
+
 tasklist | find /i "mpv.exe"
 
-if errorlevel 0 (
+if %errorlevel% == 0 (
 	for /F "tokens=2 skip=3" %%i in ('tasklist /FI "IMAGENAME eq mpv.exe"') do (
-		if errorlevel 0 (
+		if %errorlevel% == 0 (
 			cscript %APPDATA%\mpv\mpv_quit.vbs %%i
-			
+			goto lunchmpv
 		)
 	)
-	powershell Start-Sleep -m 1000
 )
 
+:lunchmpv
+
 start "" "C:\ProgramData\chocolatey\lib\mpv.install\tools\mpv.exe" %*
+
+
--- mpv_quit.vbs
+++ mpv_quit.vbs
@@ -4,7 +4,7 @@ set pid = WScript.Arguments
 if pid.count <> 0 then
 	WSShell.AppActivate(Cint(pid(0)))
 	WSShell.SendKeys "%{F4}" 'ALT+F4
-	' WScript.Sleep 750 '終了失敗する場合はSleepを挟む
+	' WScript.Sleep 250 '終了失敗する場合はSleepを挟む
 else
 	Wscript.echo "PID NOT FOUND"
 end if
